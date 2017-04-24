7d8c1a8aec368c909058cc94ba557df99e183846
Martin BjÃrkstrÃm <martin.bjorkstrom@gmail.com>
2017-04-24 13:56:56 +0300

    Add signature help

---
+++ src/Cake.OmniSharp/RequestHandlers/Signatures/SignatureHelpHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Composition;
+using OmniSharp;
+using OmniSharp.Mef;
+using OmniSharp.Models;
+
+namespace Cake.OmniSharp.RequestHandlers.Signatures
+{
+    [OmniSharpHandler(OmnisharpEndpoints.SignatureHelp, Constants.LanguageNames.Cake), Shared]
+    public class SignatureHelpHandler : CakeRequestHandler<SignatureHelpRequest, SignatureHelp>
+    {
+        [ImportingConstructor]
+        public SignatureHelpHandler(OmniSharpWorkspace workspace) : base(workspace)
+        {
+        }
+    }
+}
